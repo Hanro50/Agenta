30a128b0ff10e1ff853138683caa40d7b7d54bfe
Hanro <hlareman2@gmail.com>
2023-03-05 12:06:34 +0200

    version bump

--- src/main/java/mod_Agenta.java
+++ src/main/java/mod_Agenta.java
@@ -1,7 +1,7 @@
 public class mod_Agenta extends BaseMod {
     static {za.net.hanro50.mod.Commom.load("legacy modloader");}
     public String getName() {return "Agenta";}
-    public String getVersion() {return "1.6.0";}
+    public String getVersion() {return "1.6.1";}
     public String Version() {return getVersion();}
     public void load() {}
 }
