af7923996460276df8c779d2d3fb0071790fc44b
Hanro <hlareman2@gmail.com>
2023-03-06 21:28:05 +0200

    I'm going to tinker with this....

---
+++ wrapart/src/main/java/za/net/hanro50/App.java
@@ -0,0 +1,13 @@
+package za.net.hanro50;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
