ee6504233ead6435c1631658ab9c7eb325e532cc
Hanro <hlareman2@gmail.com>
2023-03-07 17:07:00 +0200

    version bump

--- agenta/src/main/java/za/net/hanro50/mod/Agenta.java
+++ agenta/src/main/java/za/net/hanro50/mod/Agenta.java
@@ -2,7 +2,7 @@ package za.net.hanro50.mod;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = "Agenta", name = "Agenta", version = "1.6.2", useMetadata = true)
+@Mod(modid = "Agenta", name = "Agenta", version = "1.7.1", useMetadata = true)
 public class Agenta {
     static {
             Commom.load("Forge");
