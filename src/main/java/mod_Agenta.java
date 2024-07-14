918d917e20e17dee9ce761fcb8ba0dc422cfa0a0
Hanro <hlareman2@gmail.com>
2024-07-14 09:44:39 +0200

    reorganisation

--- src/main/java/mod_Agenta.java
+++ src/main/java/mod_Agenta.java
@@ -2,7 +2,7 @@
 public class mod_Agenta extends BaseMod {
     static {za.net.hanro50.mod.Commom.load("legacy modloader");}
     public String getName() {return "Agenta";}
-    public String getVersion() {return "1.7.1";}
+    public String getVersion() {return getClass().getPackage().getImplementationVersion();}
     public String Version() {return getVersion();}
     public void load() {}
 }
--- src/main/java/za/net/hanro50/mod/Agenta.java
+++ src/main/java/za/net/hanro50/mod/Agenta.java
@@ -1,7 +1,7 @@
 package za.net.hanro50.mod;
 
 import cpw.mods.fml.common.Mod;
-@Mod(modid = "Agenta", name = "Agenta", version = "1.7.1", useMetadata = true)
+@Mod(modid = "Agenta", name = "Agenta", version = "1.8.1", useMetadata = true)
 public class Agenta {
     static {
             Commom.load("Forge");
